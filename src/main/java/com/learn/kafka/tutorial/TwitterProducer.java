ca14d93f2c299ce10f329ca76de33cc81b848d75
Sasi Kumar T <sasikt@thoughtworks.com>
2020-06-30 12:37:33 +0530

    Added a twitter producer handle

---
+++ src/main/java/com/learn/kafka/tutorial/TwitterProducer.java
@@ -0,0 +1,80 @@
+package com.learn.kafka.tutorial;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.BasicClient;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+
+    public TwitterProducer() {
+
+    }
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    public void run() {
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100);
+
+        BasicClient client = createTwitterClient(msgQueue);
+        client.connect();
+
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null) {
+                logger.info(msg);
+            }
+        }
+
+        logger.info("End of application");
+
+
+    }
+
+    private BasicClient createTwitterClient(BlockingQueue<String> msgQueue) {
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+
+/** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+// Optional: set up some followings and track terms
+        ArrayList<String> terms = Lists.newArrayList("tiktok");
+        hosebirdEndpoint.trackTerms(terms);
+
+// These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1("53S20N2lmMD4pnWydhnhGfI6G", "oDG1vBaonZy2eh1ebqF0du0oC06OWqIdZyxgDZMIhnhzuBmO5C", "83569893-kvQ8YHSajR6iksNlyAFFGyvLOLnIhUViNUhRirSyi", "wdoByQOZZCLBHrvGjRK0QkKBJ8n3OkoXjr1lZWjtAQoFU");
+
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        return builder.build();
+    }
+}
